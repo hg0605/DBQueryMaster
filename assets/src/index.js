23c4e16559e8c86ad18ad1cc10f9c2a81c5dbbf0
Nitish Jaiswal <nitishj023@gmail.com>
2018-06-30 15:44:53 +0530

    React-Sails Basic Setup

---
+++ assets/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>DBQuery Master</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
